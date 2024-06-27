b59c5d9beb50d3df428e152509636d8f5d3be9ce
Vinicius Paiva <viniciuspaiva434@gmail.com>
2024-06-26 23:49:01 -0300

    codigo do desafio 22 finalizado

--- Desafio-22.py
+++ Desafio-22.py
@@ -1,3 +1,9 @@
 nome = input('digite seu nome\n')
 
-print(nome)
+nomeSplit = nome.split()
+ 
+print('seu nome em letras em maiusculas:', nome.upper(), '\n')
+print('seu nome em letras minusculas:  ', nome.lower() ,'\n')
+print('a quantidade letras em seu nome:   ', len(''.join(nome.split())), '\n')
+print('quantas letras tem seu primeiro nome: ', len(nomeSplit[0]), '\n')
+
